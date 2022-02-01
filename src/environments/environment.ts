2af21a6d09779d6ebb3f2cbc216b32cd1e682ba3
Asmae CHAH <asmaechah30@gmail.com>
2022-02-02 00:24:49 +0100

    commit change for deploy heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiURL: 'http://localhost:8010/api/'
+  //apiURL: 'http://localhost:8010/api/'
+  apiURL:'https://backend-assignment-app.herokuapp.com/api/'
 };
 
 /*
